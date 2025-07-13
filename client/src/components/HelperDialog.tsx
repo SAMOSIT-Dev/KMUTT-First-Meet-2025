cca21cf00fe6c69c6dffb93f94afa855522deec7
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-13 20:08:19 +0700

    refactor: improve layout and update helper context

--- client/src/components/HelperDialog.tsx
+++ client/src/components/HelperDialog.tsx
@@ -1,10 +1,14 @@
 import { Dialog, Toggle } from "radix-ui";
 import { motion } from "framer-motion";
 import { CircleQuestionMark, Languages, X } from "lucide-react";
-import { cn } from "../libs/utils";
 import { useEffect, useState } from "react";
-import { mapCardTypeToIcon } from "./Card";
-import { cardTypes } from "../libs/constants";
+
+import { cn } from "../libs/utils";
+
+import HeartIcon from "../assets/icons/Heart";
+import DiamondIcon from "../assets/icons/Diamond";
+import SpadeIcon from "../assets/icons/Spade";
+import ClubIcon from "../assets/icons/Club";
 
 type Language = "EN" | "TH";
 
@@ -44,98 +48,80 @@ function LanguageSelection({
 
 function ThaiContent() {
   return (
-    <div className="mt-4 font-kanit">
+    <div className="mt-4 font-poppins">
       <div className="space-y-2">
         <h1 className="font-semibold tracking-wider text-xl">วิธีการเล่น</h1>
-        <p className="text-sm">
-          รูปแบบของการ์ดจะมีทั้งหมด 12 ตัวเลขและ 4 สัญลักษณ์
+        <p>
+          รูปแบบของการ์ดจะมีทั้งหมด 12 ตัวเลขและ 4 สัญลักษณ์{"  "}
+          <span className="inline-flex items-center align-middle *:size-4 space-x-1">
+            <HeartIcon className="text-red-500" />{" "}
+            <DiamondIcon className="text-red-500" /> <SpadeIcon /> <ClubIcon />
+          </span>
         </p>
-        <div className="flex items-center w-fit mt-2 flex-wrap gap-2 bg-neutral-200 p-2 rounded-lg">
-          {cardTypes.map((type) => (
-            <div
-              key={type}
-              className="flex items-center justify-center size-10"
-            >
-              {mapCardTypeToIcon[type]}
-            </div>
-          ))}
-        </div>
-        <h2 className="font-semibold tracking-wider text-lg">วิธีการได้แต้ม</h2>
         <div>
-          <p className="font-medium">1. Pair</p>
-          <p className="text-sm">
-            ต้องจับกลุ่มให้มีเลขเหมือนกัน 2 ใบ ทั้งหมด 2 ชุด
-          </p>
+          <p className="font-semibold">1. Pair</p>
+          <p>ต้องจับกลุ่มให้มีเลขเหมือนกัน 2 ใบ ทั้งหมด 2 ชุด</p>
         </div>
         <div>
-          <p className="font-medium">2. Three of a Kind</p>
-          <p className="text-sm">จับกลุ่มให้มีเลขเหมือนกัน 3 ทั้งหมด 2 ชุด</p>
+          <p className="font-semibold">2. Three of a Kind</p>
+          <p>ต้องจับกลุ่มให้มีเลขเหมือนกัน 3 ทั้งหมด 2 ชุด</p>
         </div>
         <div>
-          <p className="font-medium">3. Four of a Kind</p>
-          <p className="text-sm">จับกลุ่มให้มีเลขเหมือนกัน 4 ทั้งหมด 2 ชุด</p>
+          <p className="font-semibold">3. Four of a Kind</p>
+          <p>ต้องจับกลุ่มให้มีเลขเหมือนกัน 4 ทั้งหมด 2 ชุด</p>
         </div>
         <div>
-          <p className="font-medium">4. Straight</p>
-          <p className="text-sm">จับกลุ่มให้ตัวเลขเรียงกัน 5 ใบ</p>
+          <p className="font-semibold">4. Straight</p>
+          <p>ต้องจับกลุ่มให้ตัวเลขเรียงกันทั้งหมด 5 ใบ</p>
         </div>
         <div>
-          <p className="font-medium">5. Flush</p>
-          <p className="text-sm">จับกลุ่มให้ดอกเดียวกัน 5 ใบ</p>
+          <p className="font-semibold">5. Flush</p>
+          <p>ต้องจับกลุ่มให้สัญลักษณ์เดียวกันทั้งหมด 5 ใบ</p>
         </div>
       </div>
     </div>
   );
 }
 
-function EnglishConten() {
+function EnglishContent() {
   return (
     <div className="mt-4 font-poppins">
       <div className="space-y-2">
         <h1 className="font-bold tracking-wide text-xl">How to Play</h1>
-        <p className="text-sm">
-          The cards consist of 12 numbers and 4 symbols.
+        <p>
+          The cards consist of 12 numbers and 4 symbols.{"  "}
+          <span className="inline-flex items-center align-middle *:size-4 space-x-1">
+            <HeartIcon className="text-red-500" />{" "}
+            <DiamondIcon className="text-red-500" /> <SpadeIcon /> <ClubIcon />
+          </span>
         </p>
-        <div className="flex items-center w-fit mt-2 flex-wrap gap-2 bg-neutral-200 p-2 rounded-lg">
-          {cardTypes.map((type) => (
-            <div
-              key={type}
-              className="flex items-center justify-center size-10"
-            >
-              {mapCardTypeToIcon[type]}
-            </div>
-          ))}
-        </div>
-        <h2 className="font-bold tracking-wide text-lg mt-4">How to Score</h2>
         <div>
           <p className="font-semibold">1. Pair</p>
-          <p className="text-sm">
+          <p>
             Group two identical cards together. Two sets of two cards with the
             same number.
           </p>
         </div>
         <div>
           <p className="font-semibold">2. Three of a Kind</p>
-          <p className="text-sm">
+          <p>
             Group three cards with the same number. Two sets of three cards
             each.
           </p>
         </div>
         <div>
           <p className="font-semibold">3. Four of a Kind</p>
-          <p className="text-sm">
+          <p>
             Group four cards with the same number. Two sets of four cards each.
           </p>
         </div>
         <div>
           <p className="font-semibold">4. Straight</p>
-          <p className="text-sm">
-            Group five cards in consecutive number order.
-          </p>
+          <p>Group five cards in consecutive number order.</p>
         </div>
         <div>
           <p className="font-semibold">5. Flush</p>
-          <p className="text-sm">Group five cards with the same symbol.</p>
+          <p>Group five cards with the same symbol.</p>
         </div>
       </div>
     </div>
@@ -152,7 +138,7 @@ export default function HelperDialog() {
           whileHover={{ backgroundColor: "oklch(0.92 0 0)" }}
           whileTap={{ scale: 0.9 }}
           className={cn(
-            "fixed right-0 size-14 top-0 m-4 flex flex-col items-center justify-center p-2 cursor-pointer z-50",
+            "fixed right-0 size-14 bottom-0 m-4 flex flex-col items-center justify-center p-2 cursor-pointer z-50",
             "rounded-full bg-neutral-50/90"
           )}
         >
@@ -171,7 +157,9 @@ export default function HelperDialog() {
               <X size={30} />
             </Dialog.Close>
           </Dialog.Title>
-          <div className="h-full min-h-0 overflow-y-auto">{lanaguage === "TH" ? <ThaiContent /> : <EnglishConten />}</div>
+          <div className="h-full min-h-0 overflow-y-auto">
+            {lanaguage === "TH" ? <ThaiContent /> : <EnglishContent />}
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
