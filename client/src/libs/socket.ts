38def6aa94caff020dc393753a8aca90ac767caa
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-14 19:08:38 +0700

    init: initialize ongoing project

--- client/src/libs/socket.ts
+++ client/src/libs/socket.ts
@@ -3,9 +3,8 @@ import { io, Socket } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "ws://localhost:3000";
 
 const socketOptions = {
-  // path: "/",
   autoConnect: false,
-  transports: ["polling", "websocket"],
+  transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
