eda3ceb57676f15cbb66de0526fe8134f518a6e6
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-14 19:14:27 +0700

    chore: normalize git

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const io = new Server(server, {
     origin: ALLOWED_ORIGINS,
     credentials: true,
   },
-  transports: ["polling", "websocket"],
+  transports: ["websocket"],
 });
 
 instrument(io, {
