4c11ac1c704d80333dbbe6690b806d27e4954adb
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-14 22:10:56 +0700

    fix: resolve incorrect strat target

--- server/pm2.script.js
+++ server/pm2.script.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "socket-server",
-      script: "./server.js",
+      script: "server.js",
       instances: "max",
       exec_mode: "cluster",
       env: {
