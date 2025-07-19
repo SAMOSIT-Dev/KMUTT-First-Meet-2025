0fa92a1f8ed855182aaa061cd15a487998f4c99c
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-19 17:09:04 +0700

    fix: layout improvement

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,15 @@ import { useCard } from "./hooks/useCard";
 import StatusIndicator from "./components/StatusIndicator";
 
 export default function App() {
-  const { cardNumber = 0, cardType = "", status = "pending" } = useCard()
+  const { cardNumber, cardType, status = "pending" } = useCard()
   return (
     <Container>
-      <StatusIndicator status={status} />
+      <div className="fixed m-4 top-0 left-0">
+        <StatusIndicator status={status} />
+      </div>
       <HelperDialog />
       <div className="fixed top-1/2 left-1/2 -translate-1/2 h-[90dvh] w-[90dvw] max-w-lg flex flex-col">
-        <Card allowTapping type={cardType as CardType} number={cardNumber} />
+        <Card allowTapping={!!cardNumber && !!cardType} type={cardType as CardType} number={cardNumber} />
       </div>
     </Container>
   );
