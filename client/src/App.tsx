74fd31d4d8fe1c5889193e4df606bf59828d0265
Kittihengcharoen Chaemprasert <KINNOM-Cyber@users.noreply.github.com>
2025-07-19 17:05:05 +0700

    fix: resolve websocket error and add indicator status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,13 @@ import Container from "./components/Container";
 import Card, { type CardType } from "./components/Card";
 import HelperDialog from "./components/HelperDialog";
 import { useCard } from "./hooks/useCard";
+import StatusIndicator from "./components/StatusIndicator";
 
 export default function App() {
-  const { cardNumber, cardType } = useCard()
-  
+  const { cardNumber = 0, cardType = "", status = "pending" } = useCard()
   return (
     <Container>
+      <StatusIndicator status={status} />
       <HelperDialog />
       <div className="fixed top-1/2 left-1/2 -translate-1/2 h-[90dvh] w-[90dvw] max-w-lg flex flex-col">
         <Card allowTapping type={cardType as CardType} number={cardNumber} />
--- client/src/libs/socket.ts
+++ client/src/libs/socket.ts
@@ -5,6 +5,7 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "ws://localhost:3000";
 const socketOptions = {
   autoConnect: false,
   transports: ["websocket"],
+  path: "/service/socket",
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 1000,
